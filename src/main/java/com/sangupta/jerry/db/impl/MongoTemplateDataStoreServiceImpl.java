67e122ef476841bd8d05dda1ca9503d1719f9214
Sandeep Gupta <sandy.pec@gmail.com>
2020-08-31 10:02:33 -0700

    Update MongoTemplate based data store [ci skip]

--- src/main/java/com/sangupta/jerry/db/impl/MongoTemplateDataStoreServiceImpl.java
+++ src/main/java/com/sangupta/jerry/db/impl/MongoTemplateDataStoreServiceImpl.java
@@ -11,127 +11,29 @@ import org.springframework.data.mongodb.core.query.Query;
 
 import com.mongodb.client.result.DeleteResult;
 import com.sangupta.jerry.db.DataStoreService;
-import com.sangupta.jerry.util.AssertUtils;
 
 /**
  * {@link MongoTemplate} based implementation of the {@link DataStoreService}.
+ * 
+ * <pre>
+ * DataStoreService<MyEntity, String> service = new MongoTemplateDataStoreServiceImpl<MyEntity, String>() {};
+ * </pre>
+ * 
+ * Now service can be used for all CRUD operations on <code>MyEntity</code> object
+ * using <code>service</code>.
  * 
  * @author sangupta
  * 
  * @since 2.0.0
  *
- * @param <T>
+ * @param <T> The entity object type which is persisted in the datastore
  * 
- * @param <X>
+ * @param <X> The primary ID key for this entity object
  */
 public abstract class MongoTemplateDataStoreServiceImpl<T, X> extends AbstractDataStoreServiceImpl<T, X> {
-
+    
     @Inject
     protected MongoTemplate mongoTemplate;
-
-    @Override
-    public T get(X primaryID) {
-        if (primaryID == null) {
-            return null;
-        }
-
-        return this.mongoTemplate.findById(primaryID, this.entityClass);
-    }
-
-    @Override
-    public List<T> getMultiple(Collection<X> ids) {
-        if (AssertUtils.isEmpty(ids)) {
-            return null;
-        }
-
-        Query query = new Query(Criteria.where(this.idKeyFieldName).in(ids));
-        return this.mongoTemplate.find(query, this.entityClass);
-    }
-    
-    @Override
-    public List<T> getMultiple(X[] ids) {
-        if (AssertUtils.isEmpty(ids)) {
-            return null;
-        }
-
-        Query query = new Query(Criteria.where(this.idKeyFieldName).in(ids));
-        return this.mongoTemplate.find(query, this.entityClass);
-    }
-
-    @Override
-    public List<T> getAll() {
-        return this.mongoTemplate.findAll(this.entityClass);
-    }
-
-    @Override
-    public List<T> getAll(int page, int pageSize) {
-        Query query = new Query();
-        query.limit(pageSize);
-        if (page > 1) {
-            query.skip((page - 1) * pageSize);
-        }
-        return this.mongoTemplate.find(query, this.entityClass);
-    }
-
-    @Override
-    public boolean insertEntity(T entity) {
-        X primaryID = this.getPrimaryID(entity);
-        if (primaryID != null) {
-            if (!this.allowEmptyOrZeroID() && AssertUtils.isEmpty(primaryID)) {
-                return false;
-            }
-        }
-
-        try {
-            this.mongoTemplate.insert(entity);
-        } catch (RuntimeException e) {
-            // this ensures that any insert operation that fails returns a false
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean updateEntity(T entity) {
-        X primaryID = getPrimaryID(entity);
-        if (primaryID == null) {
-            return false;
-        }
-
-        if (!allowEmptyOrZeroID() && AssertUtils.isEmpty(primaryID)) {
-            return false;
-        }
-
-        this.mongoTemplate.save(entity);
-        return true;
-    }
-
-    @Override
-    public boolean upsertEntity(T entity) {
-        X primaryID = getPrimaryID(entity);
-        if (primaryID == null) {
-            this.mongoTemplate.save(entity);
-            return true;
-        }
-
-        if (!allowEmptyOrZeroID() && AssertUtils.isEmpty(primaryID)) {
-            return false;
-        }
-
-        this.mongoTemplate.save(entity);
-        return true;
-    }
-
-    @Override
-    public T delete(X primaryID) {
-        if (primaryID == null) {
-            return null;
-        }
-
-        Query query = new Query(Criteria.where(this.idKeyFieldName).is(primaryID));
-        return this.mongoTemplate.findAndRemove(query, this.entityClass);
-    }
 
     @Override
     public long count() {
@@ -143,14 +45,90 @@ public abstract class MongoTemplateDataStoreServiceImpl<T, X> extends AbstractDa
         this.mongoTemplate.dropCollection(this.entityClass);
     }
 
-    /**
-     * Whether to allow <code>empty</code> primary key values
-     * in the entity.
-     * 
-     * @return
-     */
-    protected boolean allowEmptyOrZeroID() {
-        return false;
+    @Override
+    protected T getEntity(X primaryID) {
+        return this.mongoTemplate.findById(primaryID, this.entityClass);
+    }
+
+    @Override
+    protected List<T> getMultipleEntities(Collection<X> ids) {
+        Query query = new Query(Criteria.where(this.entityDetails.idFieldName).in(ids));
+        return this.mongoTemplate.find(query, this.entityClass);
+    }
+    
+    @Override
+    protected List<T> getMultipleEntities(X[] ids) {
+        Query query = new Query(Criteria.where(this.entityDetails.idFieldName).in(ids));
+        return this.mongoTemplate.find(query, this.entityClass);
+    }
+
+    @Override
+    protected List<T> getAllEntities() {
+        return this.mongoTemplate.findAll(this.entityClass);
+    }
+
+    @Override
+    protected List<T> getAllEntities(int page, int pageSize) {
+        Query query = new Query();
+        query.limit(pageSize);
+        if (page > 1) {
+            query.skip((page - 1) * pageSize);
+        }
+        return this.mongoTemplate.find(query, this.entityClass);
+    }
+
+    @Override
+    protected T insertEntity(T entity) {
+        return this.mongoTemplate.insert(entity);
+    }
+
+    @Override
+    protected T updateEntity(T entity) {
+        X primaryID = getPrimaryID(entity);
+        if (primaryID == null) {
+            return null;
+        }
+
+        return this.mongoTemplate.save(entity);
+    }
+
+    @Override
+    protected T upsertEntity(T entity) {
+        X primaryID = getPrimaryID(entity);
+        if (primaryID == null) {
+            this.mongoTemplate.save(entity);
+            return null;
+        }
+
+        return this.mongoTemplate.save(entity);
+    }
+    
+    @Override
+    protected T deleteEntity(T entity) {
+        DeleteResult result = this.mongoTemplate.remove(entity);
+        if(result != null && result.getDeletedCount() > 0) {
+            return entity;
+        }
+        
+        return null;
+    }
+
+    @Override
+    protected T deleteEntityForID(X primaryID) {
+        Query query = new Query(Criteria.where(this.entityDetails.idFieldName).is(primaryID));
+        return this.mongoTemplate.findAndRemove(query, this.entityClass);
+    }
+
+    @Override
+    protected List<T> deleteMultipleEntities(Collection<X> ids) {
+        Query query = new Query(Criteria.where(this.entityDetails.idFieldName).in(ids));
+        return this.mongoTemplate.findAllAndRemove(query, this.entityClass);
+    }
+
+    @Override
+    protected List<T> deleteMultipleEntities(X[] ids) {
+        Query query = new Query(Criteria.where(this.entityDetails.idFieldName).in(ids));
+        return this.mongoTemplate.findAllAndRemove(query, this.entityClass);
     }
 
 }
